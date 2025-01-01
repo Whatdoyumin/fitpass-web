e75b787897b038ce2563809eae14b5c7cf32877f
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-01-02 02:17:30 +0900

    feat: 비밀번호 수정 퍼블리싱만 진행 시작작

--- src/pages/ChangePassword.tsx
+++ src/pages/ChangePassword.tsx
@@ -1,5 +1,49 @@
-function ChangePssword() {
-  return <div>비밀번호 변경 페이지</div>;
+interface ChangePasswordProps {
+  currentPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
 }
 
-export default ChangePssword;
+function ChangePassword({
+  currentPassword,
+  newPassword,
+  confirmNewPassword
+}: ChangePasswordProps) {
+  return (
+      <div className="bg-white p-6">
+        {/* 기존 비밀번호 */}
+        <div className="mb-[10px]">
+          <label className="text-gray-800 text-[16px] font-bold">기존 비밀번호</label>
+          <input
+            type="password"
+            value={currentPassword}
+            placeholder="비밀번호를 입력해주세요"
+            className="w-full p-3 border border-gray-300 rounded-md mt-[10px] text-[14px] font-bold"
+          />
+        </div>
+
+        {/* 새로운 비밀번호 */}
+        <div className="mb-[227px]">
+          <label className="text-gray-800 text-[16px] font-bold">새로운 비밀번호</label>
+          <input
+            type="password"
+            value={newPassword}
+            placeholder="새로운 비밀번호를 입력해주세요"
+            className="w-full p-3 border border-gray-300 rounded-md mt-[10px] text-[14px] font-bold"
+          />
+          <input
+            type="password"
+            value={confirmNewPassword}
+            placeholder="새로운 비밀번호를 재확인해주세요"
+            className="w-full p-3 border border-gray-300 rounded-md mt-[10px] text-[14px] font-bold"
+          />
+        </div>
+        {/* 변경하기 버튼 */}
+        <button className="w-full max-w-[350px] h-[51px] bg-blue-500 text-white rounded-lg text-[15px] font-bold">
+          변경하기
+        </button>
+      </div>
+  );
+}
+
+export default ChangePassword;
--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+
 function EditProfile() {
-  return <div>프로필 수정 페이지</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-[360px] h-[270px] p-[30px_20px] bg-white rounded-[7px] flex justify-center items-center">
+        <div className="flex flex-col">
+          {/* 비밀번호 변경 */}
+          <div>
+            <p className="text-gray-800 text-[16px] font-bold">비밀번호 변경</p>
+            <div className="h-[15px]" /> {/* 15px 공백 */}
+            <Link to="/my/change-password">
+              <button className="w-[320px] h-[51px] bg-blue-500 text-white rounded-lg text-[15px] font-bold">
+                비밀번호 변경하기
+              </button>
+            </Link>
+          </div>
+
+          {/* 전화번호 변경 */}
+          <div>
+          <p className="text-gray-800 text-[16px] mt-[40px] font-bold">전화번호 변경</p>
+          <div className="h-[15px]" /> {/* 15px 공백 */}
+            <Link to="/my/change-phone">
+              <button className="w-[320px] h-[51px] bg-blue-500 text-white rounded-lg text-[15px] font-bold">
+                전화번호 변경하기
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EditProfile;
