5c7e1a467d1635382c0525c9baf539544d5050be
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-01-08 22:36:00 +0900

    feat: 화면 높이 변경 중중

--- src/pages/EditProfile/EditProfile.tsx
+++ src/pages/EditProfile/EditProfile.tsx
@@ -2,7 +2,7 @@ import EditProfileCommon from "./EditProfileCommon";  // ChangeButton 컴포넌�
 
 function EditProfile() {
   return (
-    <div className="h-screen overflow-hidden bg-white-200 flex justify-center items-start pt-[29px] pr-[15px] pl-[15px]">
+    <div className="h-full bg-white-200 flex justify-center items-start pt-[29px] pr-[15px] pl-[15px]">
       <div
         className="w-[360px] h-[270px] px-5 py-[30px] bg-white-100 rounded-lg flex flex-col justify-center items-center gap-[40px]"
         style={{ boxShadow: '0px 4px 20px 0px #EAEAEA' }}
