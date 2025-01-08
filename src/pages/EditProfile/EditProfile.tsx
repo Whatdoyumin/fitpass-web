07657ab6b2aa31d81e7783b57b44cd79e0ac2615
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-01-08 22:22:41 +0900

    feat: 개인정보 수정 페이지 퍼블리싱싱

---
+++ src/pages/EditProfile/EditProfile.tsx
@@ -0,0 +1,17 @@
+import EditProfileCommon from "./EditProfileCommon";  // ChangeButton 컴포넌트를 import
+
+function EditProfile() {
+  return (
+    <div className="h-screen overflow-hidden bg-white-200 flex justify-center items-start pt-[29px] pr-[15px] pl-[15px]">
+      <div
+        className="w-[360px] h-[270px] px-5 py-[30px] bg-white-100 rounded-lg flex flex-col justify-center items-center gap-[40px]"
+        style={{ boxShadow: '0px 4px 20px 0px #EAEAEA' }}
+      >
+        <EditProfileCommon text="비밀번호 변경" linkTo="/my/change-password" />
+        <EditProfileCommon text="전화번호 변경" linkTo="/my/change-phone" />
+      </div>
+    </div>
+  );
+}
+
+export default EditProfile;
