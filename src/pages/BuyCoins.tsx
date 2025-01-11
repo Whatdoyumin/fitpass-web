278738710d2952841806d2f322590431e04ec98b
Whatdoyumin <lym4963@naver.com>
2025-01-12 06:38:04 +0900

    feat: 코인 요금 안내 페이지 구현

--- src/pages/BuyCoins.tsx
+++ src/pages/BuyCoins.tsx
@@ -1,5 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { CoinCard } from "../components/buyCoin/CoinCard";
+import { COIN_PRICE } from "../constants/price-menu";
+
 function BuyCoins() {
-  return <div>코인 구매하기 페이지</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-full h-full bg-white-200 overflow-y-auto py-4">
+      <div className="w-full h-full flex flex-col gap-6 items-center">
+        <div className="w-full h-full flex flex-col gap-4 justify-center items-center">
+          {COIN_PRICE.map((item) => (
+            <CoinCard
+              key={item.id}
+              coinAmount={item.coinAmount}
+              coinPrice={item.coinPrice}
+              coinExp={item.coinExp}
+              isBgBlue={item.isBgBlue}
+            />
+          ))}
+        </div>
+        <button className="w-[340px] blueButton py-3" onClick={() => navigate("payment")}>
+          코인 구매하기
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default BuyCoins;
