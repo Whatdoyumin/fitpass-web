1001dbcb956b8a0b0821acbbc411d2a12f75a287
Whatdoyumin <lym4963@naver.com>
2025-01-11 17:57:10 +0900

    feat: 위치 설정 페이지 화면별 상태 적용

--- src/pages/SetLocation.tsx
+++ src/pages/SetLocation.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import { SetLocationOption } from "../components/setLocation/SetLocationOption";
+import { SearchLocation } from "../components/setLocation/SearchLocation";
+import GetCurrentLocation from "../components/setLocation/GetCurrentLocation";
 
 function SetLocation() {
   const [mode, setMode] = useState<"default" | "my-location" | "search">("default");
 
-  return <div>{mode === "default" && <SetLocationOption setMode={setMode} />}</div>;
+  return (
+    <div>
+      {mode === "default" && <SetLocationOption setMode={setMode} />}
+      {mode === "search" && <SearchLocation />}
+      {mode == "my-location" && <GetCurrentLocation />}
+    </div>
+  );
 }
 
 export default SetLocation;
