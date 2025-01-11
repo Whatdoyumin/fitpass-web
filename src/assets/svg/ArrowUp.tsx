a4142ff90d7ba96991062b1fd4640ed8de7ca291
Whatdoyumin <lym4963@naver.com>
2025-01-12 06:29:29 +0900

    chore: 위, 아래 화살표 아이콘 추가

---
+++ src/assets/svg/ArrowUp.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgArrowUp = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 10" {...props}>
+    <path stroke="#BABABA" strokeLinecap="round" strokeWidth={2} d="m1 9 7.5-7L16 9" />
+  </svg>
+);
+export default SvgArrowUp;
