eb7a1f4009bc79aea87577879919f7310cb888e1
Whatdoyumin <lym4963@naver.com>
2025-01-17 22:53:54 +0900

    feat: 현재 위치 설정 후 로컬스토리지 저장, 홈으로 이동

--- src/components/setLocation/GetCurrentLocation.tsx
+++ src/components/setLocation/GetCurrentLocation.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { useGeoLocation } from "../../hooks/useGeoLocation";
+import { useNavigate } from "react-router-dom";
 
 const geolocationOptions = {
   enableHighAccuracy: true,
@@ -7,9 +9,20 @@ const geolocationOptions = {
 };
 
 const GetCurrentLocation = () => {
+  const navigate = useNavigate();
   const { location, error } = useGeoLocation(geolocationOptions);
 
-  return <div>{(location.latitude, location.longitude)}</div>;
+  useEffect(() => {
+    if (location?.latitude && location.longitude) {
+      localStorage.setItem("lat", String(location?.latitude));
+      localStorage.setItem("lng", String(location?.longitude));
+      navigate("/");
+    } else if (error) {
+      alert("위치 정보를 가져오는 데 실패했습니다. 다시 시도해주세요.");
+    }
+  });
+
+  return <></>;
 };
 
 export default GetCurrentLocation;
