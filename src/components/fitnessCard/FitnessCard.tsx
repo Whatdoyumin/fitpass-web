030ee4888d0b3dcee6c77fd5dd07e98e12e71aeb
fsdffds <qhals_01@naver.com>
2025-01-30 13:51:56 +0900

    style: 공통 컴포넌트 완성

--- src/components/fitnessCard/FitnessCard.tsx
+++ src/components/fitnessCard/FitnessCard.tsx
@@ -26,25 +26,29 @@ function FitnessCard({ fitness }: FitnessCardProps) {
         // 기본 텍스트 및 스타일 (기본 상태를 "사용 가능"으로 설정)
         let statusText = "사용 가능"; // 기본 상태를 "사용 가능"으로 설정
         let statusStyle = ""; // 기본 스타일
+        let bgStyle = "bg-white-100" // 기본 배경색
+        let circleBg = "bg-white-200"  // 반원
 
         // fee가 있을 경우
         if (item.fee !== undefined) {
           statusText = `${item.fee}코인`; 
           statusStyle = "border border-blue-400" // fee가 있을 경우 코인 텍스트
         } else if (item.status) {
-          // status가 있을 경우
+          // status가 있을 경우 -> 패스 사용하기 페이지
           switch (item.status) {
-            case "none":
+            case "NONE":
               statusText = "사용 가능";
               statusStyle = "w-16 border-2 border-blue-400"
               break;
-            case "progress":
+            case "PROGRESS":
               statusText = "사용 중";
               statusStyle = "w-14 border-2 border-blue-400";
               break;
-            case "done":
+            case "DONE":
               statusText = "리뷰 남기기";
               statusStyle = "w-[70px] h-[26px] border bg-blue-500 text-white-100 border-blue-500 rounded-[5px] ";
+              bgStyle = "bg-gray-200"
+              circleBg = "bg-gray-300"
               break;
             default:
               statusText = "사용 가능"; // 사실 없는 경우..
@@ -52,18 +56,20 @@ function FitnessCard({ fitness }: FitnessCardProps) {
           }
         }
 
-        const formattedDate = item.status === "done" && item.activeTime ? myDate(item.activeTime) : "";
+        const formattedDate = item.status === "DONE" && item.activeTime ? myDate(item.activeTime) : "";
 
         return (
-          <div className="flex rounded bg-white-100 relative h-[117px]" key={index}
+          <div className={`flex rounded relative h-[117px] ${bgStyle}`} key={index}
             onClick={() => {
-              handleSaveToLocalStorage({
-                fitnessId: item.fitnessId ?? 0,
-                name: item.fitnessName,
-                distance: item.distance,
-                imageUrl: item.imageUrl ?? ""
-              });
-              navigate(`/fitness/${item.fitnessId}`)
+              if (item.fee !== undefined) {
+                handleSaveToLocalStorage({
+                  fitnessId: item.fitnessId ?? 0,
+                  name: item.fitnessName,
+                  distance: item.distance,
+                  imageUrl: item.imageUrl ?? ""
+                });
+                navigate(`/fitness/${item.fitnessId}`)
+              }
             }}>
             <img className="w-[117px] rounded-l"
               src={item.imageUrl}
@@ -84,8 +90,8 @@ function FitnessCard({ fitness }: FitnessCardProps) {
                 <img
                   src={Done}
                   alt="사용 완료"
-                  className="absolute bottom-0 left-0 z-10"/>
-              ) }
+                  className="absolute bottom-[5px] left-[50px] z-10"/>
+              )}
             <div className="flex flex-col w-[190px] h-[84px]">
               <div className="p-4 rounded-r-[5px]">
                 <div className="w-[177px] h-[58px] flex flex-col gap-[10px]">
@@ -102,12 +108,24 @@ function FitnessCard({ fitness }: FitnessCardProps) {
                     <p className="text-[10px] text-gray-600 ">{item.distance}km</p>
                   </div>
                 </div>
-                {/* <div className={`absolute flex items-center bottom-4 right-4 ${statusStyle} border rounded-[15px] w-15 h-[22px] text-[10px] font-bold text-center py-[5px] px-[10px]`}> */}
-                <div className={`absolute flex justify-center items-center w-15 bottom-4 right-4 text-blue-400 rounded-[15px] h-[22px] text-[10px] font-bold text-center py-[5px] px-[10px] ${statusStyle} `}>
-                  {statusText}
+                {/* 버튼 */}
+                <div className="absolute bottom-4 right-4 flex items-center gap-[12px] ">
+                  { statusText === "사용 중" && (
+                    <span className="text-blue-500 text-[10px] font-bold">20:10</span>
+                  ) }
+                  <div className={`flex justify-center items-center w-15 text-blue-400 rounded-[15px] h-[22px] text-[10px] font-bold text-center py-[5px] px-[10px] ${statusStyle} `}
+                    onClick={() => {
+                      if (statusText === "리뷰 남기기") {
+                        navigate("/upload-review")
+                      }
+                    }}>
+                    {statusText}
+                  </div>
                 </div>
+                {/* <div className={`absolute flex items-center bottom-4 right-4 ${statusStyle} border rounded-[15px] w-15 h-[22px] text-[10px] font-bold text-center py-[5px] px-[10px]`}> */}
+
               </div>
-              <div className="absolute top-2/4 right-[-8.5px] w-[17.05px] h-[17px] rounded-full bg-white-200 transform: -translate-y-1/2"></div>
+              <div className={`absolute top-2/4 right-[-8.5px] w-[17.05px] h-[17px] rounded-full transform: -translate-y-1/2 ${circleBg}`}></div>
             </div>
           </div>
         );
