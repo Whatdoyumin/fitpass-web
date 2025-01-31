623cf1a7ee7a4bdc6da857bb5381bb61b3379004
dhdld <btj5420@naver.com>
2025-01-31 10:39:42 +0900

    feat : 토큰 context api 전역 관리, refresh token 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import Router from "./routes/Router";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (  
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={Router} /> 
-    </QueryClientProvider>
+    <AuthProvider>
+      <RouterProvider router={Router} />
+    </AuthProvider>
   );
 }
 
--- src/hooks/useSignin.ts
+++ src/hooks/useSignin.ts
@@ -1,15 +1,19 @@
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { signIn, TSignInData } from "../apis/signin/signin";
+import { useAuth } from "../context/AuthContext";
 
 export const useSignin = () => {
   const navigate = useNavigate();
-
+  const { login } = useAuth();
+ 
   return useMutation({
     mutationFn: (data: TSignInData) => signIn(data),
     mutationKey: ["signin"],
     onSuccess: () => {
-      navigate("/");
+        console.log("로그인 성공");
+        login("accessToken", "refreshToken");
+        navigate("/");
     },
     onError: (error:Error) => {
         console.error("로그인 실패:", error.message);
