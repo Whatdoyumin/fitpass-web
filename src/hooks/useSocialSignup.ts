7b9074549013c8c07bec05e105fad2c9074e446e
dhdld <btj5420@naver.com>
2025-02-02 23:27:09 +0900

    feat : 소셜로그인 후 회원가입 api 훅

---
+++ src/hooks/useSocialSignup.ts
@@ -0,0 +1,25 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { socialLogin, TSocialLoginData } from "../apis/signup/social-login";
+
+export const useSocialSignup = () => {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  return useMutation({
+    mutationKey: ["socialSignup"],
+    mutationFn: async (data: TSocialLoginData) => {
+      const response = await socialLogin(data);
+      return response;
+    },
+    onSuccess: (data: { result: { accessToken: string; refreshToken: string } }) => {
+      console.log("✅ 소셜 회원가입 성공");
+      login(data.result.accessToken, data.result.refreshToken);
+      navigate("/");
+    },
+    onError: (error: Error) => {
+      console.error("🚨 소셜 회원가입 실패:", error.message);
+    },
+  });
+};
