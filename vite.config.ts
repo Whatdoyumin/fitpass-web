50f22f7736dd3622057d31f7225a7d07c58d81e8
dhdld <btj5420@naver.com>
2025-02-06 16:03:55 +0900

    chore : https 적용 코드 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-import mkcert from 'vite-plugin-mkcert'; // SSL 인증서 자동 생성
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    mkcert(),
     svgr({
       svgrOptions: {
         icon: true,
       },
     }),
   ],
-  server: {
-    https: true, // ✅ HTTPS 활성화
-    host: "localhost",
-    port: 5173,
-  },
 });
 
