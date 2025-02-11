db68e6ea0e3ee0db9f2d1d9c43f1942d85c99431
Whatdoyumin <lym4963@naver.com>
2025-02-11 14:25:12 +0000

    fix: maps typeError 해결을 위한 수정

--- src/pages/FitnessDetails/ReviewList.tsx
+++ src/pages/FitnessDetails/ReviewList.tsx
@@ -10,19 +10,18 @@ type ReviewsResponse = {
     reviews: TReview[];
     totalPages: number;
   };
-}
+};
 
 type fetchReviewParams = {
   offset?: number;
   pageSize?: number;
   sortBy?: string;
-}
+};
 
 function ReviewList() {
-
   const [sortOption, setSortOption] = useState<"score" | "date">("date");
-  
-  const [page, setPage] = useState(1);  // 현재 페이지
+
+  const [page, setPage] = useState(1); // 현재 페이지
   const pageSize = 3;
 
   const { id } = useParams<{ id: string }>();
@@ -32,68 +31,82 @@ function ReviewList() {
       offset: (page - 1) * pageSize,
       pageSize: pageSize,
       sortBy: sortOption,
-    }
-    const response = await axiosInstance.get(`/fitness/${id}/review`, {params});
+    };
+    const response = await axiosInstance.get(`/fitness/${id}/review`, { params });
     return response.data;
-  }
+  };
 
   const { data, refetch } = useQuery<ReviewsResponse>({
-    queryKey: ['reviews', id, sortOption, page],
+    queryKey: ["reviews", id, sortOption, page],
     queryFn: fetchReview,
-  })
+  });
 
   const handlePageChange = (newPage: number) => {
     if (newPage < 1 || newPage > (data?.result?.totalPages ?? 0)) return;
     setPage(newPage);
     if (newPage !== page) {
       refetch();
-      console.log("페이지 넘어감")
+      console.log("페이지 넘어감");
     }
-  }
+  };
 
   const sortReviews = (options: "score" | "date") => {
     setSortOption(options);
     setPage(1);
   };
 
-  return(
+  return (
     <>
       <p className="text-base font-bold">이용 후기</p>
       {/* 정렬 방식 */}
       <div className="w-[74px] h-[14px] flex items-center my-[5px] ">
-        <button className={`font-medium text-[12px] ${sortOption === "score" ? "text-black-700" : "text-gray-350"}`}
-          onClick={() => sortReviews("score")}>별점순</button>
+        <button
+          className={`font-medium text-[12px] ${
+            sortOption === "score" ? "text-black-700" : "text-gray-350"
+          }`}
+          onClick={() => sortReviews("score")}
+        >
+          별점순
+        </button>
         <div className="border-r border-black-700 h-[8px] mx-[3px] "></div>
-        <button className={`font-medium text-[12px] ${sortOption === "date" ? "text-black-700" : "text-gray-350"}`}
-          onClick={() => sortReviews("date")}>최신순</button>
+        <button
+          className={`font-medium text-[12px] ${
+            sortOption === "date" ? "text-black-700" : "text-gray-350"
+          }`}
+          onClick={() => sortReviews("date")}
+        >
+          최신순
+        </button>
       </div>
       <div className="flex flex-col gap-[15px]">
-        {data?.result?.reviews.map((review) => (
-          <ReviewItem key={review.id} review={review} refetch={refetch} />
-        ))}
+        {data?.result?.reviews &&
+          data?.result?.reviews.map((review) => (
+            <ReviewItem key={review.id} review={review} refetch={refetch} />
+          ))}
       </div>
       {/* pagination */}
       <div className="text-gray-350 w-[300px] h-[17px] gap-[12px] flex justify-center text-[14px] mt-[20px]">
+        <button onClick={() => handlePageChange(page - 1)} disabled={page === 1}>
+          {"<"}
+        </button>
+        {new Array(data?.result?.totalPages).fill(null).map((_, index) => {
+          const pageNum = index + 1;
+          return (
+            <button
+              key={pageNum}
+              className={`${page === pageNum ? "text-gray-600" : "text-gray-350"}`}
+              onClick={() => handlePageChange(pageNum)}
+            >
+              {pageNum}
+            </button>
+          );
+        })}
         <button
-          onClick={() => handlePageChange(page - 1)}
-          disabled={page === 1}>{"<"}</button>
-        {
-          new Array(data?.result?.totalPages).fill(null).map((_, index) => {
-            const pageNum = index + 1;
-            return (
-              <button
-                key={pageNum}
-                className={`${page === pageNum ? 'text-gray-600' : 'text-gray-350'}`}
-                onClick={() => handlePageChange(pageNum)}
-              >
-                {pageNum}
-              </button>
-            )
-          })
-        }
-      <button
-        onClick={() => handlePageChange(page + 1)}
-        disabled={page >= (data?.result?.totalPages ?? 0)}>{">"}</button>
+          onClick={() => handlePageChange(page + 1)}
+          disabled={page >= (data?.result?.totalPages ?? 0)}
+        >
+          {">"}
+        </button>
       </div>
     </>
   );
