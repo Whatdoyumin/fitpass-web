0d5420ef277e86a30a21b591d031f55751c9f78e
Whatdoyumin <lym4963@naver.com>
2025-02-14 16:33:24 +0900

    feat: 어드민 구매 내역 UI 구현

--- src/components/adminCommon/AdminHeader.tsx
+++ src/components/adminCommon/AdminHeader.tsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { FitpassLogo } from "../../assets/svg";
 
 const AdminHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="w-screen min-w-admin top-0 left-0 h-header bg-white-100 fixed z-10 border border-b-gray-400">
       <div className="w-full h-full flex items-center px-6">
-        <FitpassLogo width={"176px"} className="cursor-pointer" />
+        <FitpassLogo width={"176px"} className="cursor-pointer" onClick={() => navigate("/")} />
       </div>
     </header>
   );
