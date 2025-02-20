269484bd37e085300239a722b7c3750e7cf2872f
dhdld <btj5420@naver.com>
2025-02-20 14:28:40 +0900

    feat: 소셜로그인 로직

--- src/hooks/useSocialSignup.ts
+++ src/hooks/useSocialSignup.ts
@@ -1,40 +1,28 @@
 import { useMutation } from "@tanstack/react-query";
 import { useAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
 import { socialLogin } from "../apis/signup/social-login";
 
 interface TSocialSignupData {
   phoneNumber: string;
   name: string;
+  accessToken: string;
+  refreshToken: string;
 }
 
 export const useSocialSignup = () => {
-  const navigate = useNavigate();
   const { login } = useAuth();
 
   return useMutation({
     mutationKey: ["socialSignup"],
     mutationFn: async (data: TSocialSignupData) => {
-      // ✅ 쿠키에서 `accessToken` 가져오기
-      const getCookie = (name: string) => {
-        const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
-        return match ? match[2] : "";
-      };
-
-      const accessToken = getCookie("accessToken");
-      if (!accessToken) throw new Error("AccessToken이 존재하지 않습니다.");
-
-      sessionStorage.setItem("accessToken", accessToken);
+      sessionStorage.setItem("accessToken", data.accessToken);
+      login(data.accessToken, data.refreshToken);
 
       const response = await socialLogin(data);
       return response;
     },
-    onSuccess: (data: { result: { accessToken: string; refreshToken: string; memberRole: string } }) => {
+    onSuccess: (data) => {
       console.log("✅ 소셜 회원가입 성공", data);
-      login(data.result.accessToken, data.result.refreshToken);
-
-      if (data.result.memberRole === "ADMIN") navigate("/admin");
-      else navigate("/");
     },
     onError: (error: Error) => {
       console.error("🚨 소셜 회원가입 실패:", error.message);
