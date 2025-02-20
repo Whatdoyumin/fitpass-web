56717a85b3860ae7f19c93629589057273d72ec3
Whatdoyumin <lym4963@naver.com>
2025-02-20 17:06:02 +0000

    fix: 이미지 불러올 때 에러 발생 시 대체 이미지 처리

--- src/pages/Home/CardCol.tsx
+++ src/pages/Home/CardCol.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import SvgLocation from "../../assets/svg/Location";
 import { HomeCardData } from "../../types/HomeCardData";
 import { handleSaveToLocalStorage } from "../../utils/storageUtils";
+import { useState } from "react";
 
 type CardColProps = {
   data: HomeCardData;
@@ -9,8 +10,10 @@ type CardColProps = {
 
 function CardCol({ data }: CardColProps) {
   const navigate = useNavigate();
+  const fitnessImg = "https://cdn.pixabay.com/photo/2017/08/16/09/39/dumbbells-2646970_1280.jpg";
 
-  // 로컬 스토리지 저장 및 상세 페이지로 이동
+  const [imageSrc, setImageSrc] = useState(data.imageUrl || fitnessImg);
+
   const handleClickCard = () => {
     handleSaveToLocalStorage(data);
     navigate(`/fitness/${data.fitnessId}`);
@@ -19,16 +22,17 @@ function CardCol({ data }: CardColProps) {
   return (
     <div className="w-[143px] h-[140px] flex flex-col mb-0.5">
       <img
-        src={data.imageUrl}
+        src={imageSrc}
         alt="썸네일"
         className="w-full h-[107px] object-cover rounded-t overflow-hidden"
-        onClick={() => handleClickCard()}
+        onClick={handleClickCard}
+        onError={() => setImageSrc(fitnessImg)} // 이미지 로드 실패 시 대체 이미지로 변경
       />
       <div className="h-9 bg-white-100 rounded flex items-center justify-between gap-[10px] shadow-[0px_4px_20px_rgba(0,0,0,0.1)]">
-        <span className="text-[11px] font-medium text-ellipsis overflow-hidden whitespace-nowrap flex-grow pl-[10px] ">
+        <span className="text-[11px] font-medium text-ellipsis overflow-hidden whitespace-nowrap flex-grow pl-[10px]">
           {data.fitnessName}
         </span>
-        <div className="h-full flex items-center gap-1 content-center pr-[10px] ">
+        <div className="h-full flex items-center gap-1 content-center pr-[10px]">
           <SvgLocation className="w-[7px] h-[9px]" />
           <span className="text-[9px] text-gray-600">{Math.floor(data.distance)}km</span>
         </div>
