5843d80fdb39cc54a41e9fba19702b8007f6d433
Whatdoyumin <lym4963@naver.com>
2025-02-21 02:05:19 +0900

    fix: 이미지 불러올 때 에러 발생 시 대체 이미지 처리

--- src/pages/MyPage/MyProfile.tsx
+++ src/pages/MyPage/MyProfile.tsx
@@ -20,13 +20,15 @@ interface MyProfileProps {
 
 const MyProfile = ({ profile, updateProfileMutation, deleteProfileMutation }: MyProfileProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [profileImage, setProfileImage] = useState<string>(profile.pictureUrl || "none");
+  const [profileImage, setProfileImage] = useState<string>(profile.pictureUrl || ImgUser);
   const [content, setContent] = useState("default");
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const checkMobile = () => {
-      setIsMobile('ontouchstart' in window || /iPhone|iPad|iPod|Android/i.test(navigator.userAgent));
+      setIsMobile(
+        "ontouchstart" in window || /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+      );
     };
     checkMobile();
     window.addEventListener("resize", checkMobile);
@@ -39,13 +41,13 @@ const MyProfile = ({ profile, updateProfileMutation, deleteProfileMutation }: My
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
-    setContent("default"); 
+    setContent("default");
   };
 
   const handleOptionClick = async (option: string) => {
     if (option === "basic") {
       try {
-        setProfileImage(ImgUser);  // 기본 이미지로 설정
+        setProfileImage(ImgUser); // 기본 이미지로 설정
         await deleteProfileMutation();
         setContent("updated");
       } catch (error) {
@@ -106,7 +108,9 @@ const MyProfile = ({ profile, updateProfileMutation, deleteProfileMutation }: My
       {isModalOpen && (
         <div className="fixed inset-0 bg-gray-700 bg-opacity-60 flex items-center justify-center z-50">
           <div
-            className={`bg-white-100 rounded-lg ${content === "default" ? "max-w-[315px]" : "max-w-[300px]"} w-full`}
+            className={`bg-white-100 rounded-lg ${
+              content === "default" ? "max-w-[315px]" : "max-w-[300px]"
+            } w-full`}
           >
             {content === "default" ? (
               <div className="relative max-w-[315px] w-full bg-white rounded-lg pt-[20px] pr-[20px] pb-[20px] pl-[15px]">
@@ -140,7 +144,10 @@ const MyProfile = ({ profile, updateProfileMutation, deleteProfileMutation }: My
               <div className="relative w-full pt-[20px]">
                 <div className="px-[15px]">
                   <div className="flex items-center justify-center" style={{ lineHeight: "21px" }}>
-                    <button onClick={handleCloseModal} className="absolute left-[15px] text-gray-500 text-18px">
+                    <button
+                      onClick={handleCloseModal}
+                      className="absolute left-[15px] text-gray-500 text-18px"
+                    >
                       <IcCloseBtn className="w-[12px] h-[12px]" />
                     </button>
                     <p className="text-gray-800 text-18px">업로드할 사진을 골라주세요.</p>
@@ -154,7 +161,9 @@ const MyProfile = ({ profile, updateProfileMutation, deleteProfileMutation }: My
                     />
                   )}
                   {!isMobile && (
-                    <p className="text-center text-gray-500">모바일에서만 사진을 업로드할 수 있습니다.</p>
+                    <p className="text-center text-gray-500">
+                      모바일에서만 사진을 업로드할 수 있습니다.
+                    </p>
                   )}
                 </div>
               </div>
