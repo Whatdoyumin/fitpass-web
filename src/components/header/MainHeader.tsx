9371ea4d26f7bc665bca32ab8098bddb8155c122
Whatdoyumin <lym4963@naver.com>
2025-03-27 00:49:50 +0900

    fix: 헤더 레이아웃 오류 수정

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -21,18 +21,21 @@ export const MainHeader = () => {
   };
 
   return (
-    <div className="w-full h-full flex justify-center  gap-6 border-b-2 border-gray-300 pb-4">
+    <div className="w-full h-full flex justify-center  gap-6 border-b-2 border-gray-300 pt-4">
       <Logo width={"47px"} onClick={() => navigate("/")} />
-      <div className="w-[219px] h-10 bg-gray-200 flex gap-2 rounded-5 px-3 py-2">
-        <Location width={"16px"} />
-        <input
-          type="text"
-          className="w-full bg-transparent text-13px text-gray-500"
-          placeholder={localStorage.getItem("address_name") || "중구 필동로 123번지"}
-          onClick={() => navigate("/set-location")}
-        />
+      {/* 위치 설정 */}
+      <div className="flex gap-6 items-center">
+        <div className="w-[219px] h-10 bg-gray-200 flex gap-2 rounded-5 px-3 py-2">
+          <Location width={"16px"} />
+          <input
+            type="text"
+            className="w-full bg-transparent text-13px text-gray-500"
+            placeholder={localStorage.getItem("address_name") || "중구 필동로 123번지"}
+            onClick={() => navigate("/set-location")}
+          />
+        </div>
+        <Hamburger width={"26px"} onClick={toggleHambugerMenu} className="cursor-pointer" />
       </div>
-      <Hamburger width={"26px"} onClick={toggleHambugerMenu} className="cursor-pointer" />
 
       {/* 햄버거 메뉴 전체 div */}
       {openMenu && <HamburgerMenu onClick={toggleHambugerMenu} isClosing={isClosing} />}
