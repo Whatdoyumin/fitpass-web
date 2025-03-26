1fbc8e6212ea4f30d6c6cac6e5c8dffca21a1399
Whatdoyumin <lym4963@naver.com>
2025-03-27 00:26:55 +0900

    feat: 법정 정보 페이지 구현

--- src/pages/Policies/RefundPolicyPage.tsx
+++ src/pages/Policies/RefundPolicyPage.tsx
@@ -1,5 +1,16 @@
+import { REFUND_POLICY } from "../../constants/policies";
+
 function RefundPolicyPage() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-6 p-6">
+      {REFUND_POLICY.map((policy) => (
+        <div className="flex flex-col gap-2">
+          <h2 className="text-gray-600 text-18px font-bold">{policy.title}</h2>
+          <p className="whitespace-pre-line text-gray-500 text-14px">{policy.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default RefundPolicyPage;
