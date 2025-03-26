55e6667bdca212c49e070db690347e64ff21b06d
Whatdoyumin <lym4963@naver.com>
2025-03-26 16:18:08 +0000

    chore: 햄버거 메뉴 아이콘 설정

---
+++ src/pages/Policies/PrivacyPolicyPage.tsx
@@ -0,0 +1,16 @@
+import { PRIVACY_POLICY } from "../../constants/policies";
+
+function PrivacyPolicyPage() {
+  return (
+    <div className="flex flex-col gap-6 p-6">
+      {PRIVACY_POLICY.map((policy, index) => (
+        <div className="flex flex-col gap-2" key={index}>
+          <h2 className="text-gray-600 text-18px font-bold">{policy.title}</h2>
+          <p className="whitespace-pre-line text-gray-500 text-14px">{policy.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default PrivacyPolicyPage;
