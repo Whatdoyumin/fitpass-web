f5599cc197bfb1fcfeee7496354d3520c8d1629c
Whatdoyumin <lym4963@naver.com>
2025-03-27 00:50:05 +0900

    fix: key 오류 해결

--- src/pages/Policies/RefundPolicyPage.tsx
+++ src/pages/Policies/RefundPolicyPage.tsx
@@ -3,8 +3,8 @@ import { REFUND_POLICY } from "../../constants/policies";
 function RefundPolicyPage() {
   return (
     <div className="flex flex-col gap-6 p-6">
-      {REFUND_POLICY.map((policy) => (
-        <div className="flex flex-col gap-2">
+      {REFUND_POLICY.map((policy, index) => (
+        <div className="flex flex-col gap-2" key={index}>
           <h2 className="text-gray-600 text-18px font-bold">{policy.title}</h2>
           <p className="whitespace-pre-line text-gray-500 text-14px">{policy.content}</p>
         </div>
--- src/pages/Policies/TermsPage.tsx
+++ src/pages/Policies/TermsPage.tsx
@@ -3,8 +3,8 @@ import { TERMS_OF_SERVICE } from "../../constants/policies";
 function TermsPage() {
   return (
     <div className="flex flex-col gap-6 p-6">
-      {TERMS_OF_SERVICE.map((policy) => (
-        <div className="flex flex-col gap-2">
+      {TERMS_OF_SERVICE.map((policy, index) => (
+        <div className="flex flex-col gap-2" key={index}>
           <h2 className="text-gray-600 text-18px font-bold">{policy.title}</h2>
           <p className="whitespace-pre-line text-gray-500 text-14px">{policy.content}</p>
         </div>
