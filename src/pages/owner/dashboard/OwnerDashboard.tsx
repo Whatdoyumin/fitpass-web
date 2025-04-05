43b8077442ec81d013f21a8c29ccc78de6360ff2
dhdld <btj5420@naver.com>
2025-04-05 16:07:31 +0900

    feat : 정산 내역 ui

--- src/pages/owner/dashboard/OwnerDashboard.tsx
+++ src/pages/owner/dashboard/OwnerDashboard.tsx
@@ -1,11 +1,16 @@
 import { IcRightArrow } from "../../../assets/svg";
+import { useNavigate } from "react-router-dom";
 
 function OwnerDashboard() {
+  const navigate = useNavigate();
+
   return (
-    <div className="w-full max-w-content flex flex-col items-center gap-[22px] relative px-5 pt-[29px] pb-[40px] bg-white-200 h-full">
+    <div className="w-full max-w-content flex flex-col items-center gap-[22px] relative px-5 pt-[29px] pb-[40px] bg-white-200">
 
     <div className="w-full flex flex-col relative gap-[15px] bg-white-100 rounded-[7px] pb-[20px]">
-      <div className="w-full flex items-center justify-end gap-[14px] px-[20px] pt-[15px]">
+      <div className="w-full flex items-center justify-end gap-[14px] px-[20px] pt-[15px] cursor-pointer"
+      onClick={() => navigate("/my/notices")}
+      >
         <p className="text-[16px] text-gray-400">공지사항</p>
         <IcRightArrow className="h-[13px]" />
       </div>
@@ -29,7 +34,9 @@ function OwnerDashboard() {
     </div>
 
     <div className="w-full flex flex-col relative gap-[15px] bg-white-100 rounded-[7px] pb-[20px]">
-      <div className="w-full flex items-center justify-end gap-[14px] px-[20px] pt-[15px]">
+      <div className="w-full flex items-center justify-end gap-[14px] px-[20px] pt-[15px]"
+      onClick={() => navigate("/owner/settlement-history")}
+      >
         <p className="text-[16px] text-gray-400">전체 정산 내역 보기</p>
         <IcRightArrow className="h-[13px]" />
       </div>
@@ -45,21 +52,22 @@ function OwnerDashboard() {
     </div>
 
     <div className="w-full flex flex-col relative gap-[15px] bg-white-100 rounded-[7px] pb-[20px]">
-      <div className="w-full flex items-center justify-end gap-[14px] px-[20px] pt-[15px]">
+      <div className="w-full flex items-center justify-end gap-[14px] px-[20px] pt-[15px]"
+      onClick={() => navigate("/owner/usage-history")}>
         <p className="text-[16px] text-gray-400">전체 이용 내역 보기</p>
         <IcRightArrow className="h-[13px]" />
       </div>
 
       <div className="w-full border-t border-gray-300" />
 
-      <div className="flex flex-col gap-[25px] px-[20px] text-[16px]">
+      <div className="flex flex-col gap-[25px] px-5 text-[16px]">
         {[
           { id: "아이디", date: "2025.01.02 22:30", coin: "20코인" },
           { id: "아이디", date: "2025.01.02 22:30", coin: "20코인" },
           { id: "아이디", date: "2025.01.02 22:30", coin: "20코인" },
         ].map((item, idx) => (
           <div key={idx} className="flex justify-between items-center">
-            <div className="flex flex-col">
+            <div className="flex flex-col gap-[3px]">
               <p>{item.id}</p>
               <p className="text-[12px]">{item.date}</p>
             </div>
@@ -69,7 +77,8 @@ function OwnerDashboard() {
       </div>
     </div>
 
-    <button className="mt-[14px] w-full h-[50px] bg-blue-500 rounded-5 text-[15px] text-white-100">
+    <button className="mt-[14px] w-full h-[50px] bg-blue-500 rounded-5 text-[15px] text-white-100"
+    onClick={() => navigate("/owner/facilities")}>
       시설 관리
     </button>
   </div>
