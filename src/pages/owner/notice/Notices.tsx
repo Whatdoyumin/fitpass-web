8d4f959cf037b7bc44e4e8bdefe4df9e679f0d56
dhdld <btj5420@naver.com>
2025-04-06 15:40:14 +0900

    feat : 공통 페이지네이션 사용

--- src/pages/owner/notice/Notices.tsx
+++ src/pages/owner/notice/Notices.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import SvgIcLeftPage from "../../../assets/svg/IcLeftPage";
-import SvgIcRightPage from "../../../assets/svg/IcRightPage";
+import { Pagination } from "../../../components/Pagination";
 
 const mockNotices = Array.from({ length: 28 }, (_, i) => ({
   id: i + 1,
@@ -13,21 +12,12 @@ const itemsPerPage = 10;
 
 const OwnerNotices = () => {
   const navigate = useNavigate();
-  const [currentPage, setCurrentPage] = useState(1);
-
+  const [currentPage, setCurrentPage] = useState(0); 
   const totalPages = Math.ceil(mockNotices.length / itemsPerPage);
-  const pagesPerGroup = 5;
-  const currentGroup = Math.ceil(currentPage / pagesPerGroup);
-  const startPage = (currentGroup - 1) * pagesPerGroup + 1;
-  const endPage = Math.min(startPage + pagesPerGroup - 1, totalPages);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
 
   const currentNotices = mockNotices.slice(
-    (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage
   );
 
   return (
@@ -43,9 +33,7 @@ const OwnerNotices = () => {
               className={`font-medium whitespace-nowrap ${
                 notice.noticeType === "공지사항"
                   ? "text-blue-500"
-                  : notice.noticeType === "이벤트"
-                  ? "text-red-600"
-                  : "text-gray-500"
+                  : "text-red-600"
               }`}
             >
               [{notice.noticeType === "공지사항" ? "공지" : notice.noticeType}]&nbsp;
@@ -57,38 +45,11 @@ const OwnerNotices = () => {
         ))}
       </ul>
 
-      {/* 페이지네이션 */}
-      <div className="flex justify-center items-center pt-[14px] gap-[10px] mb-[26px]">
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-          className="text-gray-350 focus:outline-none"
-        >
-          <SvgIcLeftPage width={5} />
-        </button>
-
-        {Array.from({ length: endPage - startPage + 1 }, (_, index) => (
-          <button
-            key={startPage + index}
-            onClick={() => handlePageChange(startPage + index)}
-            className={`text-sm ${
-              currentPage === startPage + index
-                ? "text-gray-600"
-                : "text-gray-350"
-            } focus:outline-none`}
-          >
-            {startPage + index}
-          </button>
-        ))}
-
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-          className="text-gray-350 focus:outline-none"
-        >
-          <SvgIcRightPage width={5} />
-        </button>
-      </div>
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={setCurrentPage}
+      />
     </div>
   );
 };
