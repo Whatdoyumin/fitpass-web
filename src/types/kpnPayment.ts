684b7bc5180d46f5570dd4f3507d543d5630aa7c
Whatdoyumin <lym4963@naver.com>
2025-04-13 19:09:46 +0900

    feat: KPN api 요청 타입 정의

---
+++ src/types/kpnPayment.ts
@@ -0,0 +1,19 @@
+export interface IKpnPaymentRequest {
+  orderName: string;
+  totalAmount: number;
+  payMethod: "CARD" | "TRANSFER" | "VIRTUAL_ACCOUNT" | "MOBILE" | "EASY_PAY";
+}
+
+export interface IKpnBillingRequest {
+  orderName?: string;
+  totalAmount?: number;
+  customerId: string;
+  fullName?: string;
+}
+
+export type TRegisteredCard = {
+  billingKey: "string";
+  bank: "string";
+  type: "string";
+  number: "string";
+};
