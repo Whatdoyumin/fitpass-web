e38d4fe4c34ac1474692e87e224069d4b00b99a9
dhdld <btj5420@naver.com>
2025-04-17 17:23:09 +0900

    chore: 불필요한 코드 제거

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Navbar from "../components/Navbar";
+import OwnerRouteGuard from "../routes/guard/OwnerRouteGuard";
 
 function RootLayout() {
   return (
+    <OwnerRouteGuard>
     <div className="h-full flex flex-col">
       <Header />
       <Outlet />
       <Navbar />
     </div>
+    </OwnerRouteGuard>
   );
 }
 
