2a29cad61a30f394891573bbf5763f7a6cca3108
Whatdoyumin <lym4963@naver.com>
2025-04-20 20:57:19 +0900

    style: height 오류 해결

--- src/pages/ExploreFitness/ExploreFitness.tsx
+++ src/pages/ExploreFitness/ExploreFitness.tsx
@@ -43,7 +43,7 @@ function ExploreFitness() {
   }
 
   return (
-    <div className="bg-white-200 px-5 pt-6 pb-navbar w-full max-w-content min-h-full absolute">
+    <div className="bg-white-200 px-5 pt-6 pb-navbar w-full max-w-content  min-h-[calc(100vh-165px)] absolute">
       <div className="flex justify-between mb-3 mt-5 px-1">
         <span className="text-[25px] font-extrabold">피트니스 시설</span>
         <div className="relative z-10">
