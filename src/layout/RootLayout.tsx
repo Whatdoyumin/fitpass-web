c030f56308b23579cc786b63f4eb616f62e9635e
dhdld <btj5420@naver.com>
2025-05-16 18:11:35 +0900

    feat: css #root RootStyleManager로 파일 변경

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Navbar from "../components/Navbar";
 import RoleRouteGuard from "../routes/guard/RoleRouteGuard";
+import RootStyleManager from "./RootStyleManager";
 
 function RootLayout() {
   return (
     <RoleRouteGuard>
+      <RootStyleManager />
       <div className="w-full h-full flex flex-col relative">
         <Header />
         <Outlet />
